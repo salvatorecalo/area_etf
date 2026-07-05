66dd5d9a74d2a794690cab0cd8a81064a6405b6d
salvo <dafder45678@gmail.com>
2026-07-05 22:42:49 +0200

    fix: make button row in desktop versiobn and column in mobile

--- app/reviews-etf/page.tsx
+++ app/reviews-etf/page.tsx
@@ -8,13 +8,15 @@ export default async function Home() {
   return (
     <div className={style.page}>
       <SearchPanel with_text={true} />
-      <Link href="/reviews-etf/bachelor-courses" className={style.link}>
+      <div className={style.links}>
+        <Link href="/reviews-etf/bachelor-courses" className={style.link}>
           Elettronica Triennale
       </Link>
         
       <Link href="/reviews-etf/master-courses" className={style.link}>
           Elettronica Magistrale
       </Link>
+      </div>
     </div>
   );
 }
